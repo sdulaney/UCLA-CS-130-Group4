0cbcb6fe4a31112a6b4137cd8064c795ae04a5db
Jonathan Khamis <jonathan.khamis95@gmail.com>
2020-11-05 13:28:30 -0800

    frontend sekelton

---
+++ web/src/app.js
@@ -0,0 +1,5 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import ChickenTinderApp from "./components/ChickenTinderApp";
+
+ReactDOM.render(<ChickenTinderApp />, document.getElementById("app"));
---
+++ web/src/components/ChickenTinderApp.js
@@ -0,0 +1,13 @@
+import React from "react";
+import Action from "./Action";
+import Header from "./Header";
+
+export default class ChickenTinderApp extends React.Component {
+  render() {
+    return (
+      <div>
+        <h1>ChickenTinderApp</h1>
+      </div>
+    );
+  }
+}
