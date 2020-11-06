78dac34c1468d6f8d9b1e4e3e1d325a9d498155f
Jonathan Khamis <jonathan.khamis95@gmail.com>
2020-11-06 11:00:12 -0800

    Fix webpack bug

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,4 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import ChickenTinderApp from "./components/ChickenTinderApp";
-import "./styles/styles.scss";
 ReactDOM.render(<ChickenTinderApp />, document.getElementById("app"));
--- client/webpack.config.js
+++ client/webpack.config.js
@@ -13,7 +13,6 @@ module.exports = {
         test: /\.js$/,
         exclude: /node_modules/,
       },
-      { test: /\.scss$/, use: ["style-loader", "css-loader", "sass-loader"] },
     ],
   },
   devtool: "cheap-module-eval-source-map",
