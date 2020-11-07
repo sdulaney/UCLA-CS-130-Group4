be08bd9305608dcca8a4cfecf9eecbe3d5f22a77
Jonathan Khamis <jonathan.khamis95@gmail.com>
2020-11-06 16:30:37 -0800

    Create dropdown input field component

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,10 +1,12 @@
 import React from "react";
 import InputField from "./InputField";
+import DropdownField from "./DropdownField";
+import { us_states } from "../config";
 const App = () => {
   return (
     <div>
       <InputField fieldName={"test 1"}></InputField>
-      <InputField fieldName={"test 1"}></InputField>
+      <DropdownField inputList={us_states} fieldName={"test 2"}></DropdownField>
     </div>
   );
 };
