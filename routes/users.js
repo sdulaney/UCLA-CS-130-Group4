80398f609d4cc22160e5d5f4c8fa7f97f6a32b55
sitao chen <sitao977g@gmail.com>
2020-11-12 18:31:25 -0800

    update response and redis comment

--- routes/users.js
+++ routes/users.js
@@ -8,23 +8,40 @@ const client = redis.createClient();
 /* GET unique url*/
 router.get('/', function(req, res, next) {
 	const uid = uuid.v4();
-	//TODO: avoid duplicates
+	//TODO: generate an entry in the db
 	res.status(200).json({groupid:uid});
 
 });
 
 /* POST user to a group*/
 router.post('/:uid/:username', function(req, res, next){
-	//TODO: other than redis?
 	const uid = req.params.uid;
 	const username = req.params.username;
-	// add user to group
-	client.on('connect', function() {
-		console.log('connected')
-	})
+	// TODO: add user to group
+	// const client = redis.createClient();
+	// client.on('connect', function() {
+	// 	console.log('connected')
+	// })
 
-	client.rpush(uuid, username);
+	// client.rpush(uuid, username);
 	res.status(200);
+})
+
+/* GET users from a group*/
+router.get('/:uid', function(req, res, next){
+	//TODO: other than redis?
+	const uid = req.params.uid;
+	// TODO: get users from a group
+	// const client = redis.createClient();
+	// client.on('connect', function() {
+	// 	console.log('connected')
+	// })
+
+	// client.get(uuid, function(err, reply){
+		// if (err) then throw err;
+	// 	res.status(200).json(reply);
+	// });
+	// res.status(200);
 })
 
 module.exports = router;
