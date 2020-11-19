95cdd5b749981333e1a0bbd9c78b2c666c6961b8
sitao chen <sitao977g@gmail.com>
2020-11-19 14:20:52 -0800

    post user to a group

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 const port = (process.env.PORT || 3001)
+const uuid = require('uuid');
 
 var createError = require('http-errors');
 var express = require('express');
@@ -11,6 +12,14 @@ var azure = require('./azure.js');
 var request = require('request');
 var cheerio = require('cheerio');
 
+//------redis-------------------
+const redis = require('redis');
+const client = redis.createClient();
+
+client.on('connect', function() {
+	console.log('connected')
+})
+
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
@@ -20,10 +29,10 @@ var app = express();
 // app.set('views', path.join(__dirname, 'views'));
 // app.set('view engine', 'jade');
 
-var mongoose = require('mongoose');
-var mongodb_uri;
+// var mongoose = require('mongoose');
+// var mongodb_uri;
 
-mongoose.connect(mongodb_uri, {useNewUrlParser: true, useUnifiedTopology: true});
+// mongoose.connect(mongodb_uri, {useNewUrlParser: true, useUnifiedTopology: true});
 
 app.use(logger('dev'));
 app.use(express.json());
@@ -35,6 +44,50 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', indexRouter);
 //TODO: change /users to ?
 app.use('/users', usersRouter);
+
+/* POST user to a group*/
+app.post('/:uid/:username', (req,res) =>{
+	const uid = req.params.uid;
+	const username = req.params.username;
+	const userid = uuid.v4();
+	console.log("in post "+userid);
+	// client.hmset(uid, "username", username);
+	client.hget(uid, "usernames", function(err,reply){
+		if(err){
+			res.status(404).send(err);
+		}else{
+			var obj = JSON.parse(reply); // username JSON array
+			console.log(obj);
+			obj.push(userid);
+			client.hset(uid, "usernames", JSON.stringify(obj));
+			client.hset(userid, "name", username, "likedRestaurants", '[]');
+			// res.status(200).send(reply);
+		}
+	});
+	console.log("after post "+userid);
+	client.hget(uid, "fetchedRestaurants", function(err,reply){
+		if(err){
+			res.status(404).send(err);
+		}else{
+			res.status(200).send(JSON.parse(reply));
+		}
+	});
+	// res.status(200).end();
+})
+
+/* GET user to a group*/
+app.get('/:uid', (req,res) =>{
+	const uid = req.params.uid;
+	console.log(uid);
+	// const username = req.params.username;
+	client.get(uid, function(err,reply){
+		if(err){
+			res.status(404).send(err);
+		}else{
+			res.status(200).send(reply);
+		}
+	});
+})
 
 app.post("/api/summarizeurl", (req, res) => {
 	if(!req.body.url)
--- routes/users.js
+++ routes/users.js
@@ -3,31 +3,39 @@ var router = express.Router();
 const uuid = require('uuid');
 const redis = require('redis');
 const client = redis.createClient();
+// var groupModel = require('./groupSchema')
 
 
-/* GET unique url*/
-router.get('/', function(req, res, next) {
-	const uid = uuid.v4();
-	//TODO: generate an entry in the db
-	res.status(200).json({groupid:uid});
+// /* GET unique url*/
+// router.get('/', function(req, res, next) {
+// 	const uid = uuid.v4();
+// 	//TODO: generate an entry in the db
+// 	res.status(200).json({groupid:uid});
 
-});
+// });
 
 /* POST user to a group*/
 router.post('/:uid/:username', function(req, res, next){
-	const uid = req.params.uid;
-	const username = req.params.username;
-	// TODO: add user to group
-	// const client = redis.createClient();
-	// client.on('connect', function() {
-	// 	console.log('connected')
-	// })
+	// const uid = req.params.uid;
+	// const username = req.params.username;
+	// // TODO: add user to group
 
 	// client.rpush(uuid, username);
-	res.status(200);
-})
 
-/* GET users from a group*/
+	// client.get(uuid, function(err, reply){
+	// 	if (err){
+	// 		console.log(err);
+	// 		res.status(404);
+	// 	}
+	// 	res.status(200).json(reply);
+	// });
+
+	// var findquery = {"groupId":{uid}}
+
+	res.status(200);
+});
+
+/* GET restaurant from a group*/
 router.get('/:uid', function(req, res, next){
 	//TODO: other than redis?
 	const uid = req.params.uid;
@@ -41,6 +49,14 @@ router.get('/:uid', function(req, res, next){
 		// if (err) then throw err;
 	// 	res.status(200).json(reply);
 	// });
+	// const query = {"groupId":{uid}}
+	// groupModel.findOne(query, function(err, data{
+	// 	if(err){
+	// 		console.log(err);
+	// 	}else{
+	// 		res.status(200).send(data);
+	// 	}
+	// }))
 	// res.status(200);
 })
 
