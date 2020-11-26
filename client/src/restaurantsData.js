0a52a1e28116fedd31b8f0d02f37b7e1159be163
Jonathan Khamis <jonathan.khamis95@gmail.com>
2020-11-26 14:51:47 -0800

    Make cards swippable

---
+++ client/src/restaurantsData.js
@@ -0,0 +1,22 @@
+const resturants = [
+  {
+    resturantName: "In-N-Out",
+    distance: "10 miles",
+    imageUrl:
+      "https://i.insider.com/512b7fa3ecad04e11c000001?width=1100&format=jpeg&auto=webp",
+  },
+  {
+    resturantName: "Sushi",
+    distance: "10 miles",
+    imageUrl:
+      "https://images.japancentre.com/recipes/pics/18/main/makisushi.jpg?1557308201",
+  },
+  {
+    resturantName: "Italian",
+    distance: "10 miles",
+    imageUrl:
+      "https://belleofthekitchen.com/wp-content/uploads/2019/09/one-pot-cheesy-italian-sausage-spaghetti-square-4.jpg",
+  },
+];
+
+export default resturants;
