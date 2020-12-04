3b3e05b1589648a0ba87b162c009790e04d05eb8
Stewart Dulaney <sdulaney@users.noreply.github.com>
2020-12-04 00:22:27 -0800

    Clean up app.js

--- app.js
+++ app.js
@@ -7,10 +7,8 @@ var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 var summary = require('./summary.js');
 var bodyParser = require('body-parser');
-var azure = require('./azure.js');
 var request = require('request');
 var cheerio = require('cheerio');
-var ioRedis = require('ioredis');
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var groupRouter = require('./routes/groups');
@@ -26,11 +24,6 @@ var restaurants = classes.restaurants
 
 const { exit } = require('process');
 
-// view engine setup
-// app.set('views', path.join(__dirname, 'views'));
-// app.set('view engine', 'jade');
-
-const client = new ioRedis()
 app.use(cors());
 app.use(logger('dev'));
 
@@ -45,6 +38,8 @@ app.use('/join', usersRouter);
 app.use('/groups', groupRouter);
 app.use('/match', matchRouter);
 app.use('/swipe', swipeRouter);
+
+////////// START: Routes to create seed data for demo/testing purposes //////////
 
 //initializes redis with some dummy data
 //userId: 1234
@@ -114,104 +109,13 @@ app.get('/populate-dummy-data', async (req, res) => {
 	res.status(200).send('dummy data successfully populated')
 })
 
-app.post("/api/summarizeurl", (req, res) => {
-	if(!req.body.url)
-  		return res.send("[ERROR] Missing field 'url'");
-
-  	var url = req.body.url;
-  	requestPromise({
-  		url: url
-  	}).then(data => {
-  		var $ = cheerio.load(data);
-  		var num = 2;
-		if(req.body.num && parseInt(req.body.num) >= 1)
-			num = parseInt(req.body.num);
-
-		summarize($("body").text().replace(/\s+/g, " "), num, (data) => {
-			res.json(data);
-		});
-  	});
-});
-
-
-app.post("/api/summarize", (req, res) => {
-  if(!req.body.text)
-  	return res.send("[ERROR] Missing field 'text'");
-
-  var text = req.body.text;
-  var num = 2;
-  if(req.body.num && parseInt(req.body.num) >= 1)
-  	num = parseInt(req.body.num);
-
-  summarize(text, num, (data) => {
-  	res.json(data);
-  })
-});
-
-function summarize(text, num, cb) {
-	var response = {};
-  	summary.summarize("", text, num, function(err, summary) {
-	    response.summary = summary;
-	    var body = JSON.stringify({
-	  		"documents": [{
-			    "language": "en",
-			    "id": "1",
-			    "text": text.substring(0, 4000) //max char limit in azure
-			}]
-		});
-		var headers = {
-			'Ocp-Apim-Subscription-Key': azure.key,
-			'Content-type': 'application/json'
-		}
-
-	    var promises = [];
-	    promises.push(requestPromise({
-	    	url: "https://westus.api.cognitive.microsoft.com/text/analytics/v2.0/entities",
-	    	headers: headers,
-	  		body: body
-	    }));
-	    promises.push(requestPromise({
-	    	url: "https://westus.api.cognitive.microsoft.com/text/analytics/v2.0/keyPhrases",
-	    	headers: headers,
-	  		body: body
-	    }));
-	    promises.push(requestPromise({
-	    	url: "https://westus.api.cognitive.microsoft.com/text/analytics/v2.0/sentiment",
-	    	headers: headers,
-	  		body: body
-	    }));
-
-	    Promise.all(promises).then(values => {
-	    	console.log(JSON.parse(values[0]));
-	    	response.entities = JSON.parse(values[0]).documents[0].entities;
-	    	response.keyPhrases = JSON.parse(values[1]).documents[0].keyPhrases;
-	    	response.sentiment = JSON.parse(values[2]).documents[0].score;
-
-	    	cb(response);
-	    });
-  	});
-}
-
-function requestPromise(options) {
-    return new Promise(function(resolve, reject){
-        request.post(options, (err, resp, body) => {
-            if (err) 
-            	return reject(err);
-            try {
-                resolve(body);
-            } 
-            catch(e) {
-                reject(e);
-            }
-        });
-    });
-}
+////////// END: Routes to create seed data for demo/testing purposes //////////
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
   next(createError(404));
 });
 
-app.listen(port, () => console.log(`Example app listening on port ${port}!`))
+app.listen(port, () => console.log(`Chicken Tinder listening on port ${port}!`))
 
 module.exports = app;
